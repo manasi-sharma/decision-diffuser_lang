27e3e0b0191332df4de268fddad41d28fc36b233
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-04-17 00:25:58 -0700

    update

--- code/diffuser/datasets/sequence.py
+++ code/diffuser/datasets/sequence.py
@@ -155,6 +155,7 @@ class CondSequenceDataset(torch.utils.data.Dataset):
         self.normalize()
 
         print(fields)
+        import pdb;pdb.set_trace()
         # shapes = {key: val.shape for key, val in self.fields.items()}
         # print(f'[ datasets/mujoco ] Dataset fields: {shapes}')
 
@@ -209,10 +210,11 @@ class CondSequenceDataset(torch.utils.data.Dataset):
         trajectories = np.concatenate([actions, observations], axis=-1)
 
         if self.include_returns:
-            rewards = self.fields.rewards[path_ind, start:]
+            """rewards = self.fields.rewards[path_ind, start:]
             discounts = self.discounts[:len(rewards)]
             returns = (discounts * rewards).sum()
-            returns = np.array([returns/self.returns_scale], dtype=np.float32)
+            returns = np.array([returns/self.returns_scale], dtype=np.float32)"""
+            returns = self.fields.lang[path_ind, start:][0]
             batch = RewardBatch(trajectories, conditions, returns)
         else:
             batch = Batch(trajectories, conditions)
